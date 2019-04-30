f1d932e359ccb8609a8a5d48a67cdef7c0b8c3e1
Jake <jakelovitz@gmail.com>
2019-04-30 10:36:52 -0400

    did my best

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select { |review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.map { |review| review.customer}.uniq
+  end
+
+  def average_star_rating
+    stars = self.reviews.map { |review| review.rating}
+    stars.inject{ |sum, el| sum + el }.to_f / stars.size 
+  end
+
+  def longest_review
+    reviews = self.reviews.map { |review| review.content}
+    reviews.max_by { |review| review.length}
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find { |restaurant| restaurant.name}
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
-end
 
+  attr_reader :customer, :restaurant, :content, :rating
+  @@all = []
+
+  def initialize(customer, restaurant, content, rating)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @rating = rating #unclear if I should code the rating to not exceed an integer of 5. If so, I would write a method that could check if a rating was > 5, or < 1, and set it to the appropriate integer
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
