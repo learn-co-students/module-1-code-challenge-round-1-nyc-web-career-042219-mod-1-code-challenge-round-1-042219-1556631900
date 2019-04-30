fd53060b58ca5c80c467b7c04a49c92fbdb068ce
Arslan Ali <ali.arslan319@gmail.com>
2019-04-30 11:01:40 -0400

    Did My Best

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,45 @@
+require 'pry'
+
 class Customer
+
   attr_reader :first_name, :last_name
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  #Here I am defining the class method as self.all
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, content, rating)
+    Review.new = (self, restaurant, content, rating)
+  end
+
+  def num_reviews
+
+  end
+
+  def restaurant
+     Customer.all.map { |cust| cust.restaurant == self }
+  end
+
+  def find_by_name(name)
+    full_name.split(" ")[0]
+  end
+
+  def find_all_by_first_name(name)
+  end
+
+  def all_names
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,33 @@
 class Review
-  
-end
 
+  attr_reader :customer, :restaurant
+
+  @@all = []
+
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
+  #here I am defining the class method with self.all
+  def self.all
+    @@all
+  end
+
+  def customer
+    Customer.all.select { |rv| rv.customer == self }
+  end
+
+  def restaurant
+  end
+
+  def rating
+  end
+
+  def content
+  end
+
+end
