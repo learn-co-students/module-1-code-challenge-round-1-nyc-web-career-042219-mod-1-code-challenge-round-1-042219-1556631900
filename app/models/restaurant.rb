d5c62a9505cb944c3c863450771f1d882ce905b0
Kane Koo <kanekoo@gmail.com>
2019-04-30 10:56:40 -0400

    did my best

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,58 @@
 class Restaurant
   attr_reader :name
+  @@all = []
+
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def customers
+    restarray=Review.all.map do |review|
+      review.customer if review.restaurant==self
+    end.compact
+    restarray.uniq
+  end
+
+
+  def reviews
+    revuearray=Review.all.select do |review|
+      review.restaurant==self
+    end
+    revuearray
+
+  end
+
+  def average_star_rating
+    reviewsum=0
+    self.reviews.each do |review|
+      reviewsum += review.rating
+    end
+    reviewsum/self.reviews.length
+  end
+
+  def longest_review
+    revmax=0
+    revlong=""
+    self.reviews.each do |review|
+      if revmax < review.content.length
+        revmax = review.content.length
+        revlong = review
+      end
+
+    end
+    # binding.pry
+    revlong
+
+  end
+
+
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,27 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+amy= Customer.new("Amy","Chen")
+mark=Customer.new("Mark", "Gore")
+andrea=Customer.new("Andrea","Chow")
+john=Customer.new("John", "Barron")
+omar=Customer.new("Omar", "Gee")
+
+yips=Restaurant.new("Yips")
+halal=Restaurant.new("Halal Guys")
+aroma=Restaurant.new("Aroma")
+gfg=Restaurant.new("GFG")
+
+omar.add_review(yips,"good for fidi", 3)
+omar.add_review(aroma,"worse than sbux", 1)
+amy.add_review(yips, "beats upstate", 4)
+mark.add_review(yips, "inconsistent", 2)
+mark.add_review(halal, "best ever", 5)
+mark.add_review(aroma,"fake mediterranean",1)
+mark.add_review(yips, "still inconsistent",3)
+
+puts "#{omar.full_name} did #{omar.num_reviews} reviews"
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
