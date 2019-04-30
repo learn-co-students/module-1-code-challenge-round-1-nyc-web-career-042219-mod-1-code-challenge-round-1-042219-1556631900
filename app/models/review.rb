5e8ac5c37d65fbb65f51f3bd489aa06abb075666
Kirbyasdf <kirbyAugustus@gmail.com>
2019-04-30 10:55:13 -0400

    did my best

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,40 @@
 class Review
-  
+
+attr_reader :customer, :restaurant, :content, :rating, :patron, :score, :words
+
+  @@all = []
+
+
+def self.all
+  @@all
 end
 
+
+def initialize(customer, restaurant, content, rating)
+  @customer = customer
+  @restaurant = restaurant
+  @content = content
+  @rating = rating
+  @@all << self
+  @patron = customer
+  @score = rating
+  @words = content
+end
+
+def customer
+  self.patron
+end
+
+
+def rating
+  self.score
+end
+
+def content
+  self.words
+end
+
+
+
+
+end
