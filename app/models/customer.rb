0e7fb6bf9ce4b68e3773f58e0cb64695657b33dd
Pamyk <pamyk.ch@gmail.com>
2019-04-30 11:02:39 -0400

    hopefully done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,51 @@
 class Customer
+
+    @@all = []
+
   attr_reader :first_name, :last_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+
+# Instance Methods
+def self.add_review(restaurant, content, rating)
+    new_review = Review.new(self, restaurant, content, rating)
+    new_r
+
+end
+
+
+
+def num_reviews
+    reviews.count do |review|
+    end
+end
+
+
+
+
+
+# Class Methods
+
+  def self.all
+      @@all
+  end
+  def self.all_names
+      Costumer.all.map do |name|
+          name.full_name
+  end
+    end
+        def self.find_all_by_first_name(name)
+            @@all.select.do |first_name, last_name|
+            first_name.first_name == name
+        end
+    end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,36 @@
 class Restaurant
+
+    @@all = []
+
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  # Instance Methods
+
+def costumer_review
+    costumers.map do |review|
+        review.costumers
+    end.uniq
+
+end
+#
+# def reviews
+#     Review.all.select do |review|
+#         review.restaurant == self
+# end
+
+
+
+  # Class Methods
+
+  def self.all
+      @@all
+  end
+
+  def self.find_by_name(name)
+      name.name == restaurant.name
 end
