8639f34dd419fcd22f94869d8f78fa53532d5292
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-30 10:56:30 -0400

    Did my best

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,25 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find(name)
+    binding.pry
+  end
+
+  # def customers
+  #   uniq_list = Review.all.select {|review| review.customer == self}
+  #   binding.pry
+  # end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,36 @@
 class Review
-  
-end
 
+  @@all = []
+
+  attr_reader :customer, :restaurant, :rating, :content
+
+  def initialize(customer,restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    @customer
+  end
+
+  def restaurant
+    @restaurant
+  end
+
+  def rating
+    @rating
+  end
+
+  def content
+    @content
+  end
+
+end
